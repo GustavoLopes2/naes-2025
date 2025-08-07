e9058c4c0ed5428beec730913af4bf0225b255b1
Gustavo Lopes <gustavolopesfeitosa1707@gmail.com>
2025-08-07 19:57:29 -0300

    Fix STATIC_ROOT path to use BASE_DIR for correct static file serving

--- naes2025/settings.py
+++ naes2025/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = 'static_web/'
+STATIC_ROOT = BASE_DIR / 'static_web/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
